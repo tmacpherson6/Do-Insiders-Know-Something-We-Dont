1984fe58496b6665ff1e7b8904f160529415b4f0
Thomas MacPherson <tmacphe@umich.edu>
2025-07-06 20:48:03 -0600

    creating a pipeline for automatic updates of CSV fies

---
+++ download_sec_zips.py
@@ -0,0 +1,8 @@
+"""
+First python file in our pipleine that will access the SEC database and download
+all of the Form 4 filings on record since 2006.
+"""
+
+import os
+import requests
+import pandas as pd
